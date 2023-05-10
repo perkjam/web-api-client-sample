af8b6178667eb71e936d29868475dfb610afce2d
PedroSanchez007 <junkmesend@gmail.com>
2023-05-10 12:20:01 +0100

    call test endpoints

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System.Net.Http.Headers;
 using Microsoft.Identity.Client;
-using Newtonsoft.Json;
 
 namespace ClientConsole
 {
-    public class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Making the call...");
             RunAsync().GetAwaiter().GetResult();
@@ -14,18 +13,124 @@ namespace ClientConsole
 
         private static async Task RunAsync()
         {
-            AuthConfig config = AuthConfig.ReadFromJsonFile("appsettings.json");
+            var config = AuthConfig.ReadFromJsonFile("appsettings.json");
 
-            IConfidentialClientApplication app;
+            var accessToken = await CallApiToGetToken();
+            if (string.IsNullOrEmpty(accessToken))
+                return;
 
-            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+            var httpClient = ConfigureClient(accessToken);
+            
+            // Test API Access
+            // ===============
+            Console.WriteLine("Test API access");
+            var testResponse = await httpClient.GetAsync(config.BaseAddress + "test/unauthorised");
+            // var testResponse = await httpClient.GetAsync(config.BaseAddress + "test/authorised");
+            // var testResponse = await httpClient.GetAsync(config.BaseAddress + "test/authorised-check-database-connection");
+            if (testResponse.IsSuccessStatusCode)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Successfully accessed PerkjamWebAPI");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to call the Web Api: {testResponse.StatusCode}");
+                var content = await testResponse.Content.ReadAsStringAsync();
+                Console.WriteLine($"Content: {content}");
+            }
+
+            // Post a new user
+            // ===============
+            // Console.WriteLine("Post a new user to the database");
+            // Console.WriteLine("Enter new user name for testing [example 'Donald Duck']:");
+            // var newUserName = Console.ReadLine();
+            // Console.WriteLine("Enter new user email for testing [example 'donald@gmail.com']:");
+            // var newUserEmail = Console.ReadLine();
+            // var newUserEmailAddress = new 
+            // { 
+            //     Name = newUserName, 
+            //     Email = newUserEmail, 
+            //     ClientUserId = "TeslaClientId",
+            //     config.ClientId,
+            //     config.VendorId 
+            // };
+            // HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(newUserEmailAddress));
+            // httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            //
+            // var postResponse = await httpClient.PostAsync(config.BaseAddress + "users", httpContent);
+            // if (postResponse.IsSuccessStatusCode)
+            // {
+            //     Console.ForegroundColor = ConsoleColor.Green;
+            //     Console.WriteLine("Successfully posted new user to database:");
+            // }
+            // else
+            // {
+            //     Console.ForegroundColor = ConsoleColor.Red;
+            //     Console.WriteLine($"Failed to call the Web Api and post new user to database: {postResponse.StatusCode}");
+            // }
+            // ==================
+            
+            
+            
+            // Post a new PowerUp
+            // ==================
+            // Console.WriteLine("Post a new PowerUp to the database");
+            // Console.WriteLine("Enter new client id for testing [example 123]:");
+            // var newClientId = Console.ReadLine();
+            // Console.WriteLine("Enter new user email for testing [example 'donald@gmail.com']:");
+            // var newUserEmail = Console.ReadLine();
+            // var newPowerUp = new
+            // {
+            //     ClientId = newClientId,
+            //     Email = newUserEmail
+            // };
+            // var httpContent = new StringContent(JsonConvert.SerializeObject(newPowerUp));
+            // httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            //
+            // var postResponse = await httpClient.PostAsync(config.BaseAddress + "powerups", httpContent);
+            // if (postResponse.IsSuccessStatusCode)
+            // {
+            //     Console.ForegroundColor = ConsoleColor.Green;
+            //     Console.WriteLine("Successfully posted new PowerUp to database:");
+            // }
+            // else
+            // {
+            //     Console.ForegroundColor = ConsoleColor.Red;
+            //     Console.WriteLine($"Failed to call the Web Api and post new PowerUp email to database: {postResponse.StatusCode}");
+            // }
+            // ==================
+
+            Console.ResetColor();
+        }
+
+        private static HttpClient ConfigureClient(string accessToken)
+        {
+            var result = new HttpClient();
+            var defaultRequestHeaders = result.DefaultRequestHeaders;
+
+            if (defaultRequestHeaders.Accept.All(m => m.MediaType != "application/json"))
+            {
+                result.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+            return result;
+        }
+
+        private static async Task<string> CallApiToGetToken()
+        {
+            var config = AuthConfig.ReadFromJsonFile("appsettings.json");
+            
+            var app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
                 .WithClientSecret(config.ClientSecret)
                 .WithAuthority(new Uri(config.Authority))
                 .Build();
 
-            string[] resourceIds = new string[] { config.ResourceId };
+            var resourceIds = new[] { config.ResourceId };
 
-            AuthenticationResult result = null;
+            AuthenticationResult? result = null;
             try
             {
                 result = await app.AcquireTokenForClient(resourceIds).ExecuteAsync();
@@ -41,95 +146,10 @@ namespace ClientConsole
                 Console.ResetColor();
             }
             Console.WriteLine();
-            //Console.WriteLine("Press enter to retrieve users from the Web API");
-            //Console.ReadLine();
-            //Console.WriteLine();
 
-            if (!string.IsNullOrEmpty(result?.AccessToken))
-            {
-                var httpClient = new HttpClient();
-                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
-
-                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
-                {
-                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                }
-
-                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
-
-                // Test Users functionality
-                // ========================
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Green;
-                //    string json = await response.Content.ReadAsStringAsync();
-                //    Console.WriteLine("Retrieving users from Web API:");
-                //    Console.WriteLine(json);
-                //}
-                //else
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Red;
-                //    Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
-                //    string content = await response.Content.ReadAsStringAsync();
-                //    Console.WriteLine($"Content: {content}");
-                //}
-                //Console.WriteLine();
-
-                //Console.WriteLine("Post a new user to the database");
-                //Console.WriteLine("Enter new user name for testing [example 'Donald Duck']:");
-                //var newUserName = Console.ReadLine();
-                //Console.WriteLine("Enter new user email for testing [example 'donald@gmail.com']:");
-                //var newUserEmail = Console.ReadLine();
-                //var newUserEmailAddress = new 
-                //{ 
-                //    Name = newUserName, 
-                //    Email = newUserEmail, 
-                //    ClientUserId = "TeslaClientId", 
-                //    ClientId = config.ClientId, 
-                //    VendorId = config.VendorId 
-                //};
-                //HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(newUserEmailAddress));
-                //httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                //var postResponse = await httpClient.PostAsync(config.BaseAddress + "users", httpContent);
-                //if (postResponse.IsSuccessStatusCode)
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Green;
-                //    Console.WriteLine("Successfully posted new user to database:");
-                //}
-                //else
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Red;
-                //    Console.WriteLine($"Failed to call the Web Api and post new user to database: {postResponse.StatusCode}");
-                //}
-
-                Console.WriteLine("Post a new PowerUp to the database");
-                Console.WriteLine("Enter new client id for testing [example 123]:");
-                var newClientId = Console.ReadLine();
-                Console.WriteLine("Enter new user email for testing [example 'donald@gmail.com']:");
-                var newUserEmail = Console.ReadLine();
-                var newPowerUp = new
-                {
-                    ClientId = newClientId,
-                    Email = newUserEmail
-                };
-                var httpContent = new StringContent(JsonConvert.SerializeObject(newPowerUp));
-                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                var postResponse = await httpClient.PostAsync(config.BaseAddress + "powerups", httpContent);
-                if (postResponse.IsSuccessStatusCode)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Successfully posted new PowerUp to database:");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Failed to call the Web Api and post new PowerUp email to database: {postResponse.StatusCode}");
-                }
-
-                Console.ResetColor();
-            }
+            return result == null
+                ? string.Empty
+                : result.AccessToken;
         }
     }
 }
